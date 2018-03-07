be8dd7d0c246bf9ee41712a2af171c72196a688a
Gary M. Silva <garymsilva@outlook.com>
2018-03-07 11:29:38 -0300

    Create Class: FormatoGif && FormatoBmp

---
+++ ImagemMetodoFabrica/src/metodoestatico/fabrica/FormatoBmp.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metodoestatico.fabrica;
+
+/**
+ *
+ * @author gary
+ */
+public class FormatoBmp implements Formato {
+    @Override
+    public Imagem gera() {
+        System.out.println("gera bmp");
+        return null;
+    }
+}
